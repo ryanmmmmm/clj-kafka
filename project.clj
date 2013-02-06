5b6968c18b2647f811ed0f43fbf3688d1c3a80af
rmedlin <rmedlin@rmedlinmillennialmediacoms-MacBook-Pro-2.local>
2013-02-06 15:26:58 -0800

    using local build of kafka
    --added scala 2.9.2 dependency. this eliminates collisions in my project that is using that version. unfortunately its using a local build of kafka right now.
    
    Signed-off-by: rmedlin <rmedlin@rmedlinmillennialmediacoms-MacBook-Pro-2.local>

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject clj-kafka/clj-kafka "0.0.5-0.7" 
+(defproject clj-kafka/clj-kafka "0.0.5-0.7-RWM"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojars.paul/core-kafka_2.8.0 "0.7.0-1"
+  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [kafka/kafka "2.9.1"
                   :exclusions
                   [javax.mail/mail
                    javax.jms/jms
@@ -10,6 +11,6 @@
                    jline/jline
                    net.sf.jopt-simple/jopt-simple
                    junit/junit]]
-                 [org.scala-lang/scala-library "2.8.0"]
+                 [org.scala-lang/scala-library "2.9.2"]
                  [zookeeper-clj "0.9.2"]]
   :description "Clojure wrapper for Kafka's Java API")
